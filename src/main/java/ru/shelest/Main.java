d05da81c0d22c22d824bf873b1e3bf30a93f5a6d
Denis <shel0403@gmail.com>
2022-05-23 12:48:35 +0300

    Add main method

--- src/main/java/ru/shelest/Main.java
+++ src/main/java/ru/shelest/Main.java
@@ -4,7 +4,17 @@ import ru.shelest.calculator.Calculator;
 import ru.shelest.parser.Operation;
 import ru.shelest.parser.OperationParser;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        try (final Scanner scanner = new Scanner(System.in)) {
+            System.out.println(calc(scanner.nextLine()));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
 
     public static String calc(String input) throws Exception {
         final Operation operation = OperationParser.parse(input);
